46f870a246042ec1e0d554500242712a978d7cb7
Nicolas <nicopuegher@gmail.com>
2024-11-26 21:31:04 -0300

    Fix images path

--- src/constants/initial-movies.js
+++ src/constants/initial-movies.js
@@ -1,10 +1,8 @@
-const repoName = window.location.pathname.split('/')[1] || '';
-const basePath = `/${repoName}`;
-
 const initialMovies = {
-    'The Gray Man': `${basePath}/assets/the-gray-man/the-gray-man.png`,
-    Blonde: `${basePath}/assets/blonde/blonde.png`,
-    'Minions: The Rise of Gru': `${basePath}/assets/minions-the-rise-of-gru/minions-the-rise-of-gru.png`,
+    'The Gray Man': '/assets/the-gray-man/the-gray-man.png',
+    Blonde: '/assets/blonde/blonde.png',
+    'Minions: The Rise of Gru':
+        '/assets/minions-the-rise-of-gru/minions-the-rise-of-gru.png',
 };
 
 export default initialMovies;
